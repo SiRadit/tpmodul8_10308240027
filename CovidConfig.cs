612c1d1fe5537c83a60b97abaf951c99c1f53db4
SiRadit <dharmawanraditya123@gmail.com>
2026-04-25 12:34:06 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103082400027
+{
+    internal class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_deman { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public CovidConfig LoadConfig()
+        {
+            string path = "covid_config.json";
+
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                return JsonSerializer.Deserialize<CovidConfig>(json);
+            }
+            else
+            {
+                return new CovidConfig
+                {
+                    satuan_suhu = "celcius",
+                    batas_hari_deman = 14,
+                    pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                    pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+                };
+            }
+        }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu == "celcius")
+                satuan_suhu = "fahrenheit";
+            else
+                satuan_suhu = "celcius";
+        }
+    }
+}
